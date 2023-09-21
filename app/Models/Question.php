a8784c06f8538b33f4c8298720d743bb89a325d0
Ashique <ashique12009@gmail.com>
2023-09-21 10:59:17 +0600

    migration done for question and answer

---
+++ app/Models/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Answer;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Question extends Model
+{
+    use HasFactory;
+    protected $fillable = ['question'];
+
+    public function answer() 
+    {
+        return $this->hasMany(Answer::class); 
+    }
+}
